bcc9c0a3edc745310048dd59811159d39c7c983b
KJDoles <kevin.doles@spsmail.cuny.edu>
2014-09-09 19:40:09 -0400

    Created file task_03.py.

---
+++ task_03.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''Uses raw_input function and nested conditional if statemenst to choose
+paint colors.
+'''
+
+BASE = raw_input('What base color, Seattle Gray or Manatee? ')
+
+if BASE == 'Seattle Gray':
+    ACCENT = raw_input('What accent color, Ceramic Glaze or Cumulus Nimbus? ')
+    if ACCENT == 'Ceramic Glaze':
+        HIGHLIGHT = raw_input('What highlight color, Basically White or White? ')
+    else:
+        ACCENT == 'Cumulus Nimbus'
+        HIGHLIGHT = raw_input('What highlight color, Off-White or Paper White? ')
+else:
+    BASE == 'Manatee'
+    ACCENT = raw_input('What accent color, Platinum Mist or Spartan Sage? ')
+    if ACCENT == 'Platinum Mist':
+        HIGHLIGHT = raw_input('What highlight color, Bone White or Just White? ')
+    else:
+        ACCENT == 'Spartan Sage'
+        HIGHLIGHT = raw_input('What highlight color, Fractal White or Not White? ')
+
+print 'The color choices are {}, {}, and {}.'.format(BASE, ACCENT, HIGHLIGHT)
+
+        
+            
+        
+
+    
