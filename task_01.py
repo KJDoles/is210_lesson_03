a15a7113b6d33b4a252403f503897caf392d782c
KJDoles <kevin.doles@spsmail.cuny.edu>
2014-09-09 16:50:42 -0400

    Created file task_01.py.

---
+++ task_01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''Uses raw_input function two ask two questions.
+'''
+
+QUESTION_01 = raw_input('What is your favorite color?' )
+QUESTION_02 = int(raw_input('What is your lucky number?' ))
+
