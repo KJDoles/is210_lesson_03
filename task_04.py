c831bd072f50d0b4d736502af69cea2b476d70ba
KJDoles <kevin.doles@spsmail.cuny.edu>
2014-09-14 14:14:03 -0400

    Correction to Task 04.

--- task_04.py
+++ task_04.py
@@ -7,8 +7,6 @@ DAY = raw_input('What day is it? ').lower()[0:3]
 TIME = raw_input('What time is it in a four digit number, no ":"? ')
 SNOOZE = ()
 
-if (DAY == 'sat' or 'sun') and (TIME < '0600'):
-    SNOOZE = True
-else:
-    SNOOZE = False
-    print 'Beep! Beep! Beep! Beep! Beep!'
+SNOOZE = True if DAY == ('sat','sun') or TIME < '0600' else False
+if SNOOZE == False:
+    print ('Beep! Beep! Beep! Beep! Beep!')
